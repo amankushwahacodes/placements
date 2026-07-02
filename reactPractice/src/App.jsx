2247b34436083e3d940c870c81dcef8f9c7b3ba5
Aman Kushwaha <amankushwaha7284@gmail.com>
2026-07-02 22:57:30 +0530

    Add context msg

--- reactPractice/src/App.jsx
+++ reactPractice/src/App.jsx
@@ -1,10 +1,19 @@
 import StateDemo from "./components/statedemo";
+import { UserContext } from "./components/UserContext";
+
 
 function App() {
+
+  const user = {
+    name : 'Aman Kushwaha',
+    age : 21
+  }
+
+
   return (
-    <>
+    <UserContext.Provider value={{user}} >
       <StateDemo />
-    </>
+    </UserContext.Provider>
   );
 }
 
--- reactPractice/src/components/ChildrenDemo.jsx
+++ reactPractice/src/components/ChildrenDemo.jsx
@@ -1,8 +1,22 @@
+import { useContext } from "react"
+import { UserContext } from "./UserContext"
+
 function ChildrenDemo({children}) {
+
+    const {user} =  useContext(UserContext);
+
+
     return (
+        <>
         <h1>
             {children}
+
         </h1>
+        <h2>
+            {user.name} and {user.age}
+        </h2>
+        </>
+        
     )
 }
 
